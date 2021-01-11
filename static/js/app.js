af40776764c31af493cd11bbc4bed64ae56ebbd4
Stuart Yates <syates@cmcbio.com>
2021-01-11 13:21:25 -0700

    created horizontal bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -19,14 +19,24 @@ function createBarChart() {
         .value
     d3.json('./data/samples.json')
         .then(res => {
-            const subjectData = res.samples.find(sample => sample.id == subjectID)
+            const subjectData = res.samples.find(sample => sample.id == subjectID);
+            const subjectMeta = res.metadata.find(subject => subject.id = subjectID);
             const data = [
                 {
                     x: subjectData.sample_values.slice(0, 10),
-                    y: subjectData.otu_ids.slice(0, 10)
+                    y: subjectData.otu_ids.slice(0, 10).map(id => `OTU ${id}`),
+                    text: subjectData.otu_labels.slice(0, 10).map(label => label.split(';').join(', ')),
+                    type: 'bar',
+                    orientation: 'h'
                 }
             ]
             console.log(data)
+            const layout = {
+                title: `Subject ${subjectID} (${subjectMeta.age} year old ${subjectMeta.ethnicity} ${subjectMeta.gender == 'F' ? 'female' : 'male'})`,
+                xaxis: { title: 'OTU ID' },
+                yaxis: { title: 'Value' }
+            }
+            Plotly.newPlot("bar", data, layout);
         })
         .catch(err => console.error(err))
 
