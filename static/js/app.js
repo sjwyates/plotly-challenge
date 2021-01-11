fde47eb5d288b93ab9eb191d50ff167767abce7c
Stuart Yates <syates@cmcbio.com>
2021-01-11 13:35:54 -0700

    finished bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -8,36 +8,33 @@ window.addEventListener('DOMContentLoaded', () => {
                     .node()
                     .value = name
             })
-            console.log(res)
-            createBarChart();
+            createBarChart(dropdown.node().value);
         })
 })
 
-function createBarChart() {
-    const subjectID = d3.select('#selDataset')
-        .node()
-        .value
+function createBarChart(subjectID) {
     d3.json('./data/samples.json')
         .then(res => {
             const subjectData = res.samples.find(sample => sample.id == subjectID);
-            const subjectMeta = res.metadata.find(subject => subject.id = subjectID);
+            const subjectMeta = res.metadata.find(subject => subject.id == subjectID);
             const data = [
                 {
-                    x: subjectData.sample_values.slice(0, 10),
-                    y: subjectData.otu_ids.slice(0, 10).map(id => `OTU ${id}`),
-                    text: subjectData.otu_labels.slice(0, 10).map(label => label.split(';').join(', ')),
+                    x: subjectData.sample_values.slice(0, 10).reverse(),
+                    y: subjectData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+                    text: subjectData.otu_labels.slice(0, 10).map(label => label.split(';').join(', ')).reverse(),
                     type: 'bar',
                     orientation: 'h'
                 }
             ]
-            console.log(data)
             const layout = {
-                title: `Subject ${subjectID} (${subjectMeta.age} year old ${subjectMeta.ethnicity} ${subjectMeta.gender == 'F' ? 'female' : 'male'})`,
-                xaxis: { title: 'OTU ID' },
-                yaxis: { title: 'Value' }
+                title: `Subject ${subjectID} top ${data[0].x.length} OTUs`,
+                xaxis: { title: 'Value' }
             }
-            Plotly.newPlot("bar", data, layout);
+            Plotly.react("bar", data, layout);
         })
         .catch(err => console.error(err))
+}
 
+function optionChanged(subjectID) {
+    createBarChart(subjectID)
 }
